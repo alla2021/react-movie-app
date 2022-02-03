f708ef78f684a15adb0726e85bdfe5c2edd8a591
Your Name <alla7gon@gmail.com>
2022-02-03 18:15:54 +0200

    added form

--- my-app/src/AddForm.tsx
+++ my-app/src/AddForm.tsx
@@ -10,12 +10,12 @@ import {
 function AddForm() {
   return (
     <form className="form">
-      <TextField id="outlined-basic" label="Title" variant="outlined" />
-      <TextField id="outlined-basic" label="Director" variant="outlined" />
-      <TextField id="outlined-basic" label="Duration" variant="outlined" />
-      <TextField id="outlined-basic" label="Price" variant="outlined" />
-      <TextField id="outlined-basic" label="Image" variant="outlined" />
-      <TextField id="outlined-basic" label="Description" variant="outlined" />
+      <TextField id="filled-basic" label="Title" variant="outlined" />
+      <TextField id="filled-basic" label="Director" variant="outlined" />
+      <TextField id="filled-basic" label="Duration" variant="outlined" />
+      <TextField id="filled-basic" label="Price" variant="outlined" />
+      <TextField id="filled-basic" label="Image" variant="outlined" />
+      <TextField id="filled-basic" label="Description" variant="outlined" />
       <FormGroup>
         <FormControlLabel
           control={<Checkbox defaultChecked />}
