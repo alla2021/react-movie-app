7849e144a3d9573294f53f9799350aa223957414
Your Name <alla7gon@gmail.com>
2022-02-03 17:54:36 +0200

    added form

--- my-app/src/AddForm.tsx
+++ my-app/src/AddForm.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React from "react";
+import {
+  Button,
+  FormGroup,
+  TextField,
+  Checkbox,
+  FormControlLabel,
+} from "@mui/material/";
 
 function AddForm() {
-  return <div>
-      form
-  </div>;
+  return (
+    <form className="form">
+      <TextField id="outlined-basic" label="Title" variant="outlined" />
+      <TextField id="outlined-basic" label="Director" variant="outlined" />
+      <TextField id="outlined-basic" label="Duration" variant="outlined" />
+      <TextField id="outlined-basic" label="Price" variant="outlined" />
+      <TextField id="outlined-basic" label="Image" variant="outlined" />
+      <TextField id="outlined-basic" label="Description" variant="outlined" />
+      <FormGroup>
+        <FormControlLabel
+          control={<Checkbox defaultChecked />}
+          label="featured"
+        />
+      </FormGroup>
+    </form>
+  );
 }
 
 export default AddForm;
