4127764ba68918862786eacb3866c760440ea9a4
Your Name <alla7gon@gmail.com>
2022-02-04 12:33:41 +0200

    added form

--- my-app/src/AddForm.tsx
+++ my-app/src/AddForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   Button,
   FormGroup,
@@ -8,20 +8,46 @@ import {
 } from "@mui/material/";
 
 function AddForm() {
+  const [films, setFilms] = useState([])
+  const [title, setTitle] = useState('');
+  const [director, setDirector] = useState('');
+  const [duration, setDuration] = useState('');
+  const [price, setPrice] = useState('');
+  const [img, setImg] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleAddFilmSubmit = (e) => {
+    e.preventDefault();
+    let film = {
+      _id: Math.trunc(Math.random() * 1000) + 1,
+      title,
+      director,
+      duration,
+      price,
+      img,
+      featured: false,
+      description,
+    }
+    setFilms([...films, film])
+    console.log(setFilms);
+    setTitle('');
+    setDirector('');
+    setDuration('');
+    setPrice('');
+    setImg('');
+    setDescription('');
+
+  }
+
   return (
-    <form className="form">
-      <TextField id="filled-basic" label="Title" variant="outlined" />
-      <TextField id="filled-basic" label="Director" variant="outlined" />
-      <TextField id="filled-basic" label="Duration" variant="outlined" />
-      <TextField id="filled-basic" label="Price" variant="outlined" />
-      <TextField id="filled-basic" label="Image" variant="outlined" />
-      <TextField id="filled-basic" label="Description" variant="outlined" />
-      <FormGroup>
-        <FormControlLabel
-          control={<Checkbox defaultChecked />}
-          label="featured"
-        />
-      </FormGroup>
+    <form className="form" onSubmit={handleAddFilmSubmit}>
+      <TextField id="filled-basic" label="Title" variant="outlined" value={title} onChange={(e)=>setTitle(e.target.value)}/>
+      <TextField id="filled-basic" label="Director" variant="outlined" value={director} onChange={(e)=>setDirector(e.target.value)}/>
+      <TextField id="filled-basic" label="Duration" variant="outlined" value={duration} onChange={(e)=>setDuration(e.target.value)}/>
+      <TextField id="filled-basic" label="Price" variant="outlined" value={price} onChange={(e)=>setPrice(e.target.value)}/>
+      <TextField id="filled-basic" label="Image" variant="outlined" value={img} onChange={(e)=>setImg(e.target.value)}/>
+      <TextField id="filled-basic" label="Description" variant="outlined" value={description} onChange={(e)=> setDescription(e.target.value)}/>
+      <Button color="secondary" type='submit' >Add movie</Button>
     </form>
   );
 }
